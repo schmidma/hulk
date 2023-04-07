f8bc7ff565c15b028d8d9fee502a71c38c347824
Maximilian Schmidt <maximilian@schmidt.so>
2023-04-07 16:49:36 +0300

    Remove usages of CyclerInstance

--- crates/types/src/camera_position.rs
+++ crates/types/src/camera_position.rs
@@ -1,5 +1,11 @@
-#[derive(Debug, Clone, Copy, Eq, PartialEq)]
+use serde::{Deserialize, Serialize};
+use serialize_hierarchy::SerializeHierarchy;
+
+#[derive(
+    Default, Debug, Clone, Copy, Eq, PartialEq, Serialize, Deserialize, SerializeHierarchy,
+)]
 pub enum CameraPosition {
+    #[default]
     Top,
     Bottom,
 }
